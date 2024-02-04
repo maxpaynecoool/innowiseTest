928eb0b0b16b70541314d6622213a4e40e8942b9
maxpaynecoool <goncharov789@gmail.com>
2024-02-04 19:39:43 +0300

    ffffewfr3r3/;/;/p

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import './scss/index.scss';
+
 
 const person = {
-       ewfwefewfname: "John",
+    ewfwefewfname: "John",
     age: 34,
     city: "minsk"
 
 
+
 };
 
-console.log('Slaying..ноауауауауау6о6о  .');
+console.log('Slayingrgrgrg');
